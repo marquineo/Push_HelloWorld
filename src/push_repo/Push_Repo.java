0c51da190ebda90041ca412883d9538b16a30bae
Marcos <marquineo2001@gmail.com>
2023-10-31 15:01:01 +0100

    trying an update

--- src/push_repo/Push_Repo.java
+++ src/push_repo/Push_Repo.java
@@ -6,6 +6,7 @@ public class Push_Repo {
 
     public static void main(String[] args) {
         System.out.println("Hello Github!");
+        System.out.println("Lets try an update");
     }
     
 }
